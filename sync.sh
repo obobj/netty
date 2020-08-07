823a7fbd2953f87f369308c659fd5e6542401ca5
fengmiao <fengmiao@kuaishou.com>
2020-08-08 01:10:38 +0800

    1. backup

--- sync.sh
+++ sync.sh
@@ -1,7 +1,1 @@
-git push
-git checkout origin/4.1
-git remote set-url origin https://github.com/netty/netty.git
-git pull
-git remote set-url origin https://github.com/obobj/netty.git
-git push
-git checkout learn
+git push && git checkout origin/4.1 && git remote set-url origin https://github.com/netty/netty.git && git pull && git remote set-url origin https://github.com/obobj/netty.git && git push && git checkout learn
